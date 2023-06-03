114c5f0505c1f5ad40ba982c017dab460c9deb4a
Thirupugal <133884034+thirupugal07@users.noreply.github.com>
2023-06-03 16:01:36 +0100

    Update food.js

--- food.js
+++ food.js
@@ -31,3 +31,37 @@ function changeQtyitem(){
     if(isNaN(this.value)  || this.value<1 ){
         this.value=1;
     }
+   
+   
+}
+function addcart(){
+    
+    let foods=this.parentElement;
+    let titile=foods.querySelector('.food-titile').innerHTML;
+    let price=foods.querySelector('.food-rate').innerHTML;
+    let imgsrc=foods.querySelector('.food-img').src;
+   
+    let newproductele=addcartproduct(titile,price,imgsrc);
+    let element=document.createElement('div');
+    element.innerHTML=newproductele;
+    let cartstore=document.querySelector('.cart-content');
+    cartstore.append(element);
+    LoadContent();
+}
+
+function addcartproduct(titile,price,imgsrc){
+    
+    return`<div class="cart-box">
+    <img src="${imgsrc}"class="cart-img">
+   <div class="detail-box">
+    <div class="cart-food-titile">${titile}</div>
+    <div class="price-box">
+     <div class="cart-price">${price}</div>   
+      <div class="cart-amt">${price}</div>
+    </div>
+      <input type="number" value="1" class="cart-qty">
+    </div>
+    
+    <ion-icon name="trash" class="cart-remove"></ion-icon>
+</div>`;
+}
